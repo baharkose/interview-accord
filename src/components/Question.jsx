69dec34d602c5f625de429fbb3964ed579b1e286
baharkose <baharkse17@gmail.com>
2023-12-08 12:56:56 +0300

    finished

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { arrowdown, arrowup } from "../helper/icons";
+import questions from "../helper/data";
+
 const Card = () => {
-  return (
-    <div className="card-group">
-        <div className="card">
-          <div className="ques-answer">
-            <h5>
-              Question
-            </h5>
-            <button className="btn-minus">
-              {arrowup} {arrowdown}
-            </button>
-          </div>
-          <p>answer</p>
+  const [takeId, setTakeId] = useState(null);
+
+  const handleShowButton = (id) => {
+    // Tıklanan sorunun ID'sini setTakeId ile güncelle
+    setTakeId(takeId === id ? null : id);
+  };
+
+  return questions.map(({ id, question, answer }) => (
+    <div className="card-group" key={id}>
+      <div className="card">
+        <div className="ques-answer">
+          <h5>{question}</h5>
+          <button
+            className="btn-minus"
+            onClick={() => handleShowButton(id)}
+            style={{ display: takeId === id ? "none" : "block" }}
+          >
+            {arrowdown}
+          </button>
+          <button
+            className="btn-minus"
+            onClick={() => handleShowButton(id)}
+            style={{ display: takeId === id ? "block" : "none" }}
+          >
+            {arrowup}
+          </button>
         </div>
+        <p style={{ display: takeId === id ? "block" : "none" }}>{answer}</p>
+      </div>
     </div>
-  );
+  ));
 };
 
 export default Card;
